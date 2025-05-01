a4f8ea0649b91a9c7866e6cfaaef2da4e7f4c1c3
Aleksander Wanot <aleksnader.wanot@gmail.com>
2025-05-02 00:24:11 +0200

    Add BigChartBox component and integrate it into Home layout

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -11,6 +11,7 @@ import {
 } from "../../data";
 import BarChartBox from "../../components/barChartBox/BarChartBox";
 import PieChartBox from "../../components/pieChartBox/PieChartBox";
+import BigChartBox from "../../components/bigChartBox/BigChartBox";
 
 const Home = () => {
   return (
@@ -33,7 +34,9 @@ const Home = () => {
       <div className="box box6">
         <ChartBox {...chartBoxRevenue} />
       </div>
-      <div className="box box7">Box7</div>
+      <div className="box box7">
+        <BigChartBox />
+      </div>
       <div className="box box8">
         <BarChartBox {...barChartBoxVisit} />
       </div>
