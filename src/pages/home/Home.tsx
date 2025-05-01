b27cd5d0e2df06512d9985f2bcc08cc2f475e8f7
Aleksander Wanot <aleksnader.wanot@gmail.com>
2025-05-02 00:04:38 +0200

    Add BarChartBox component and update Home layout to include bar charts

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,7 +1,15 @@
 import "./home.scss";
 import TopBox from "../../components/topBox/TopBox";
 import ChartBox from "../../components/chartBox/ChartBox";
-import { chartBoxUser } from "../../data";
+import {
+  barChartBoxRevenue,
+  barChartBoxVisit,
+  chartBoxConversion,
+  chartBoxProduct,
+  chartBoxRevenue,
+  chartBoxUser,
+} from "../../data";
+import BarChartBox from "../../components/barChartBox/BarChartBox";
 
 const Home = () => {
   return (
@@ -12,19 +20,23 @@ const Home = () => {
       <div className="box box2">
         <ChartBox {...chartBoxUser} />
       </div>
-      {/* <div className="box box3">
-        <ChartBox />
+      <div className="box box3">
+        <ChartBox {...chartBoxProduct} />
       </div>
-      <div className="box box4">
-        <ChartBox />
-      </div>
+      <div className="box box4"></div>
       <div className="box box5">
-        <ChartBox />
-      </div> */}
-      <div className="box box6">Box6</div>
+        <ChartBox {...chartBoxConversion} />
+      </div>
+      <div className="box box6">
+        <ChartBox {...chartBoxRevenue} />
+      </div>
       <div className="box box7">Box7</div>
-      <div className="box box8">Box8</div>
-      <div className="box box9">Box9</div>
+      <div className="box box8">
+        <BarChartBox {...barChartBoxVisit} />
+      </div>
+      <div className="box box9">
+        <BarChartBox {...barChartBoxRevenue} />
+      </div>
     </div>
   );
 };
