0fbe1289cdf84e89a43a8b564a79ffa75fab3e95
Aleksander Wanot <aleksnader.wanot@gmail.com>
2025-05-03 14:34:08 +0200

    Visual changes to make code cleaner

--- src/components/add/Add.tsx
+++ src/components/add/Add.tsx
@@ -1,6 +1,5 @@
 import { GridColDef } from "@mui/x-data-grid";
 import "./add.scss";
-// import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 type Props = {
   slug: string;
@@ -9,39 +8,8 @@ type Props = {
 };
 
 const Add = (props: Props) => {
-  // TEST THE API
-
-  // const queryClient = useQueryClient();
-
-  // const mutation = useMutation({
-  //   mutationFn: () => {
-  //     return fetch(`http://localhost:8800/api/${props.slug}s`, {
-  //       method: "post",
-  //       headers: {
-  //         Accept: "application/json",
-  //         "Content-Type": "application/json",
-  //       },
-  //       body: JSON.stringify({
-  //         id: 111,
-  //         img: "",
-  //         lastName: "Hello",
-  //         firstName: "Test",
-  //         email: "testme@gmail.com",
-  //         phone: "123 456 789",
-  //         createdAt: "01.02.2023",
-  //         verified: true,
-  //       }),
-  //     });
-  //   },
-  //   onSuccess: () => {
-  //     queryClient.invalidateQueries([`all${props.slug}s`]);
-  //   },
-  // });
-
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    //add new item
-    // mutation.mutate();
     console.log("added new item");
     console.log(e.target);
     props.setOpen(false);
--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -4,7 +4,6 @@ import "./Users.scss";
 import { useState } from "react";
 import { userRows } from "../../data";
 import Add from "../../components/add/Add";
-// import { useQuery } from "@tanstack/react-query";
 
 const columns: GridColDef[] = [
   { field: "id", headerName: "ID", width: 90 },
@@ -56,17 +55,6 @@ const columns: GridColDef[] = [
 
 const Users = () => {
   const [open, setOpen] = useState(false);
-
-  // TEST THE API
-
-  // const { isLoading, data } = useQuery({
-  //   queryKey: ["allusers"],
-  //   queryFn: () =>
-  //     fetch("http://localhost:8800/api/users").then(
-  //       (res) => res.json()
-  //     ),
-  // });
-
   return (
     <div className="users">
       <div className="info">
@@ -74,13 +62,6 @@ const Users = () => {
         <button onClick={() => setOpen(true)}>Add New User</button>
       </div>
       <DataTable slug="users" columns={columns} rows={userRows} />
-      {/* TEST THE API */}
-
-      {/* {isLoading ? (
-        "Loading..."
-      ) : (
-        <DataTable slug="users" columns={columns} rows={data} />
-      )} */}
       {open && <Add slug="user" columns={columns} setOpen={setOpen} />}
     </div>
   );
